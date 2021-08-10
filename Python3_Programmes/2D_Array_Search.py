732a42f569843698786e529a870383f194a75f17
Rahul Noronha <rahulnor@gmail.com>
2021-08-11 01:51:36 +0530

    added main structure

--- Python3_Programmes/2D_Array_Search.py
+++ Python3_Programmes/2D_Array_Search.py
@@ -6,15 +6,17 @@ def Search(row, col, Array, key):
                 return
     print("The element was not found",end="\n")
     return
-
-row = int(input("Enter the number of rows in the Array\n"))
-col = int(input("Enter the number of columns in the Array\n"))
-Array = list()
-print("Enter the array elements",end="\n")
-for i in range(0,row):
-    Array.append([])
-    for j in range(0,col):
-        Array[i].append(int(input()))
-key=int(input("Enter the search key element\n"))
-Search(row, col, Array, key)
+def main():
+    row = int(input("Enter the number of rows in the Array\n"))
+    col = int(input("Enter the number of columns in the Array\n"))
+    Array = list()
+    print("Enter the array elements",end="\n")
+    for i in range(0,row):
+        Array.append([])
+        for j in range(0,col):
+            Array[i].append(int(input()))
+    key=int(input("Enter the search key element\n"))
+    Search(row, col, Array, key)
                 
+if __name__=='__main__':
+    main()
